6997e8b07e125cb0ca8b4fee2838772291145824
Kamil <kamilczekotas@gmail.com>
2023-07-27 22:47:05 +0200

    Home page

--- LaravelTask/app/Models/Worker.php
+++ LaravelTask/app/Models/Worker.php
@@ -4,14 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use App\Models\User;
 
 class Worker extends Model
 {
     use HasFactory;
-
-    public function UserWorker() 
-    {
-        return $this->belongsTo(User::class);
-    }
 }
--- LaravelTask/resources/views/home.blade.php
+++ LaravelTask/resources/views/home.blade.php
@@ -11,12 +11,26 @@
                     <thead>
                         <tr>
                             <th scope="col">User</th>
+                            <th scope="col">Email</th>
                             <th scope="col">Worker</th>
-                            <th scope="col">Car</th>
                         </tr>
                     </thead>
                     <tbody>
-
+                        @foreach($Users as $User)
+                        <tr>
+                            <td>
+                                <a href="/user/{{ $User->id }}">
+                                    {{ $User->name }}
+                                </a>
+                            </td>
+                            <td>
+                                {{ $User->email }}
+                            </td>
+                            <td>
+                                {{ $User->worker->name }}
+                            </td>
+                        </tr>
+                        @endforeach
                     </tbody>
                 </table>
             </div>
