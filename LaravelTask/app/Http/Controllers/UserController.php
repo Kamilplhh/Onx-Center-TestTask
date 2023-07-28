cd792fd282d581566813845a14862a8e2f69f5f1
Kamil <kamilczekotas@gmail.com>
2023-07-28 20:59:35 +0200

    UserController and blade template

--- LaravelTask/app/Http/Controllers/UserController.php
+++ LaravelTask/app/Http/Controllers/UserController.php
@@ -2,11 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\CarLoan;
+use App\Models\Order;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     public function GetData($id) { 
-        return view('user');
+
+        $Orders = Order::where('user_id', $id)->get();
+        $Cars = CarLoan::where('user_id', $id)->get();
+
+        return view('user', compact(['Cars', 'Orders']));
     }
 }
---
+++ LaravelTask/resources/views/user.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="row justify-content-center">
+    <div class="col-md-8 py-4">
+        <div class="card">
+            <div class="card-body">
+
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">Name</th>
+                                <th scope="col">Price</th>
+                                <th scope="col">Quantity</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+
+                        </tbody>
+                    </table>               
+            </div>
+        </div>
+    </div>
+</div>
